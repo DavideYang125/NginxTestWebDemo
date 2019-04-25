3baab4b9710e7d0f7d1a0f636815c3c72c50fe75
yangww <1483722265@qq.com>
2019-04-25 10:11:37 +0800

    Nginx+IIS+asp.net mvc测试nginx负载均衡 demo

---
+++ NginxTestWebDemo/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+
+namespace NginxTestWebDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var serverName = "服务器名称：" + Server.MachineName;
+            var ip = "服务器IP地址：" + Request.ServerVariables["LOCAL_ADDR"];
+            var port = Request.ServerVariables["SERVER_PORT"];
+            var portStr = "HTTP访问端口：" + Request.ServerVariables["SERVER_PORT"];
+            var message = "hello nginx";
+            if (port == "35356") message = "good morning,Sir";
+            ViewBag.serverName = serverName;
+            ViewBag.ip = ip;
+            ViewBag.portStr = portStr;
+            ViewBag.message = message;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
